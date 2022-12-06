9e3faa8a4db10538d525781980c5071a021f8ac5
E-Vivanco <vivanco.elsa@gmail.com>
2022-12-06 19:26:17 +0000

    2° commit form-Dom

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('hello')
+//rescataremos todos los campos de tarjeta
+let tarjeta = document.querySelector('#data-card')
+let tar_v = tarjeta.children
+console.log(tar_v)
+//rescataremos todos los campos del usuario
+let usuario = document.querySelectorAll('#data-user')
+//rescataremos todos los campos de la direccion
+let direccion = document.querySelectorAll('#data-direction')
+//rescataremos todos los campos de pago
+let pago = document.querySelectorAll('#Pay-message')
