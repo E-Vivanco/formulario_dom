78aa0f6364c14707cdd5b0896e8e953cd924a622
E-Vivanco <vivanco.elsa@gmail.com>
2022-12-07 13:04:55 +0000

    act form-dom 10:04am

--- index.js
+++ index.js
@@ -4,22 +4,37 @@ let tarjeta = document.querySelector('#data-card')
 let tar_v1 =tarjeta.children[0].children[0].children[0].children[0]
 //console.log("aqui val tar_v1",tar_v1.value)
 tar_v1.addEventListener('click',function(){
-    cambia()
+    cambia(tar_v1)
 })
 let tar_v2 =tarjeta.children[0].children[1].children[0].children[0]
 tar_v2.addEventListener('click',function(){
-    cambia()
+    cambia(tar_v2)
 })
 //console.log("aqui val tar_v2",tar_v2.value)
 let tar_v3 =tarjeta.children[0].children[2].children[0].children[0]
-tar_v2.addEventListener('click',function(){
-    cambia()
+tar_v3.addEventListener('click',function(){
+    cambia(tar_v3)
 })
+function cambia(val){
+    val.style.background = "grey"
+}
 
 
-//console.log(tar_v)
 //rescataremos todos los campos del usuario
-let usuario = document.querySelectorAll('#data-user')
+let usuario = document.querySelector('#data-user')
+//console.log(usuario.children[0].children[0].children[0].children[0])
+let us_1 =usuario.children[0].children[0].children[0].children[0]
+    us_1.addEventListener('click',function(){
+        cambia(us_1)
+    })
+//console.log(us_1)
+let us_2 =usuario.children[0].children[1].children[0].children[0]
+    us_2.addEventListener('click',function(){
+        cambia(us_2)
+    })
+//console.log(us_2)
+//let us_3 =usuario.children[0].children[2].children[0].children[0]
+//console.log(us_3)
 //rescataremos todos los campos de la direccion
 let direccion = document.querySelectorAll('#data-direction')
 //rescataremos todos los campos de pago
