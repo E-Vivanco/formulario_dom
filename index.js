bcb762d4170c231768a6d3346a8af23a80ea8d60
E-Vivanco <vivanco.elsa@gmail.com>
2022-12-07 11:15:03 +0000

    act 7Dic 8:1AM

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 //rescataremos todos los campos de tarjeta
 let tarjeta = document.querySelector('#data-card')
-let tar_v = tarjeta.childNodes
-console.log(tar_v)
+let tar_v = tarjeta.querySelectorAll('input')
+tar_v.forEach((input) =>{
+    let res = ""
+    res=input.value
+    console.log(res)//.style.background="grey"
+    //console.log(input.value)
+   // console.log("input",input)
+   // console.log("i",i)
+})
+/*tar_v.addEventListener('click', function(evento){
+    evento.target.style.backgroundColor = "gray";
+})*/
+
+//console.log(tar_v)
 //rescataremos todos los campos del usuario
 let usuario = document.querySelectorAll('#data-user')
 //rescataremos todos los campos de la direccion
