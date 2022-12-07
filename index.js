89ab987452c28076e58de6142698d6f1feb9359b
E-Vivanco <vivanco.elsa@gmail.com>
2022-12-07 13:15:29 +0000

    act form-dom 10:15am

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 //rescataremos todos los campos de tarjeta
 let tarjeta = document.querySelector('#data-card')
 //console.log(tarjeta.children[0].children[0].children[0].children[0])
-let tar_v1 =tarjeta.children[0].children[0].children[0].children[0]
+let tar_v1 = tarjeta.children[0].children[0].children[0].children[0]
 //console.log("aqui val tar_v1",tar_v1.value)
 tar_v1.addEventListener('click',function(){
     cambia(tar_v1)
 })
-let tar_v2 =tarjeta.children[0].children[1].children[0].children[0]
+let tar_v2 = tarjeta.children[0].children[1].children[0].children[0]
 tar_v2.addEventListener('click',function(){
     cambia(tar_v2)
 })
 //console.log("aqui val tar_v2",tar_v2.value)
-let tar_v3 =tarjeta.children[0].children[2].children[0].children[0]
+let tar_v3 = tarjeta.children[0].children[2].children[0].children[0]
 tar_v3.addEventListener('click',function(){
     cambia(tar_v3)
 })
@@ -23,19 +23,32 @@ function cambia(val){
 //rescataremos todos los campos del usuario
 let usuario = document.querySelector('#data-user')
 //console.log(usuario.children[0].children[0].children[0].children[0])
-let us_1 =usuario.children[0].children[0].children[0].children[0]
+let us_1 = usuario.children[0].children[0].children[0].children[0]
     us_1.addEventListener('click',function(){
         cambia(us_1)
     })
 //console.log(us_1)
-let us_2 =usuario.children[0].children[1].children[0].children[0]
+let us_2 = usuario.children[0].children[1].children[0].children[0]
     us_2.addEventListener('click',function(){
         cambia(us_2)
     })
 //console.log(us_2)
-//let us_3 =usuario.children[0].children[2].children[0].children[0]
-//console.log(us_3)
+
 //rescataremos todos los campos de la direccion
-let direccion = document.querySelectorAll('#data-direction')
+let direccion = document.querySelector('#data-direction')
+let dir_1= direccion.children[0].children[0].children[0].children[0]
+    dir_1.addEventListener('click',function(){
+        cambia(dir_1)
+    })
+//console.log(dir_1)
+let dir_2= direccion.children[0].children[1].children[0].children[0]
+    dir_2.addEventListener('click',function(){
+        cambia(dir_2)
+    })
+let dir_3= direccion.children[0].children[2].children[0].children[0]
+    dir_3.addEventListener('click',function(){
+        cambia(dir_3)
+    })
+
 //rescataremos todos los campos de pago
-let pago = document.querySelectorAll('#Pay-message')
+let pago = document.querySelector('#Pay-message')
